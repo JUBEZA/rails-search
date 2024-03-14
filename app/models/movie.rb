7df680b8a9ccaa37cea1b5c7959645b18c2fd80a
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-03-14 15:26:02 -0300

    fin code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,10 +2,15 @@ class Movie < ApplicationRecord
   belongs_to :director
 
   include PgSearch::Model
-  
-  pg_search_scope :search_by_title_and_synopsis,
-    against: [ :title, :synopsis ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+  multisearchable against: [:title, :synopsis]
+
+
+  # pg_search_scope :search_by_title_and_synopsis,
+  #   against: [ :title, :synopsis ],
+  #   associated_against: {
+  #   director: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
